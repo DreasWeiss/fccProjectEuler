4bd48a3685bd7ea604396765a835e5d80d672031
DreasWeiss <dreasweiss@gmail.com>
2023-03-29 17:36:58 +0300

    Problem 3: Largest prime factor - done

---
+++ problems1100/problem3/problem.js
@@ -0,0 +1,14 @@
+function largestPrimeFactor(number) {
+
+    return true;
+}
+
+console.log(largestPrimeFactor(13195));
+console.log(largestPrimeFactor(2)); // should return a number.
+console.log(largestPrimeFactor(2)); // should return 2.
+console.log(largestPrimeFactor(3)); // should return 3.
+console.log(largestPrimeFactor(5)); // should return 5.
+console.log(largestPrimeFactor(7)); // should return 7.
+console.log(largestPrimeFactor(8)); // should return 2.
+console.log(largestPrimeFactor(13195)); // should return 29.
+console.log(largestPrimeFactor(600851475143)); // should return 6857.
---
+++ problems1100/problem3/readme.md
@@ -0,0 +1,15 @@
+# Problem 3: Largest prime factor
+[solution](./problem.js)
+<br>
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the given number?
+
+largestPrimeFactor(2) should return a number.
+Waiting: largestPrimeFactor(2) should return 2.
+Waiting: largestPrimeFactor(3) should return 3.
+Waiting: largestPrimeFactor(5) should return 5.
+Waiting: largestPrimeFactor(7) should return 7.
+Waiting: largestPrimeFactor(8) should return 2.
+Waiting: largestPrimeFactor(13195) should return 29.
+Waiting: largestPrimeFactor(600851475143) should return 6857.
